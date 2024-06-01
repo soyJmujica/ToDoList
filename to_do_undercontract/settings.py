193b9ef55427d5dbaced2721e2ef81e1fce8ce81
Jose Mujica <master1495@gmail.com>
2024-06-01 13:44:59 -0400

    static actualizado para no tomar debug

--- to_do_undercontract/settings.py
+++ to_do_undercontract/settings.py
@@ -131,12 +131,12 @@ STATICFILES_FINDERS = [
 STATIC_URL = '/static/'
 
 # This production code might break development mode, so we check whether we're in DEBUG mode
-if not DEBUG:
+"""if not DEBUG:
     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'"""
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
