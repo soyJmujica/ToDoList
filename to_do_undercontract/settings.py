6895a220644ce45f728796c8b91b770fbfdb04b9
Jose Mujica <master1495@gmail.com>
2024-06-01 13:12:27 -0400

    settings.py actualizado Debug cambiado a false

--- to_do_undercontract/settings.py
+++ to_do_undercontract/settings.py
@@ -21,12 +21,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l3dno(w)-2gt8#dq)-qcu2v)vjcvoskyr)sw!ey^np!2&mczy7'
+SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Application definition
